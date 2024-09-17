814f787c442386d53d6e7978779de2a4eccc36db
sajib37 <sajibhasan9811@gmail.com>
2024-09-18 04:50:40 +0600

    eslint setup done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import  cors  from 'cors'
-import express, { Application, Request, Response } from "express"
-const app: Application = express()
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 
 // parser
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req: Request, res : Response) => {
-  res.send('Hello World!')
-})
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
 
 export default app;

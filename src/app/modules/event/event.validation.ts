890b4c35ef0e0e705962cad4f316459999e8689a
sajib37 <sajibhasan9811@gmail.com>
2024-09-26 15:06:40 +0600

    create and update validation schema for event is created

---
+++ src/app/modules/event/event.validation.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+const ActivitiesValidation = z.object({
+    day: z.string(), 
+    activities: z.array(z.string()) 
+  });
+const createEventValidation = z.object({
+    eventCode: z.string(),
+    title:z.string(),
+    description:z.string(),
+    from:z.string(),
+    destinations: z.array(z.string()),
+    startDate:z.date(),
+    endDate:z.date(),
+    bookingDeadline:z.date(),
+    price: z.number().positive(),
+    capacity: z.number().positive(),
+    availableSlots: z.number().positive(),
+    status:z.enum(["completed", "on-going", "upcoming"]),
+    photo: z.string(),
+    category:z.enum(["Economic" , "Standard" , "Premium"]),
+    includedServices:z.array(z.string()),
+    excludedServices:z.array(z.string()),
+    requiredEquipment:z.array(z.string()),
+    itinerary: z.array(ActivitiesValidation)    
+})
+
+const updateEventValidation = z.object({
+    eventCode: z.string().optional(),
+    title:z.string().optional(),
+    description:z.string().optional(),
+    from:z.string().optional(),
+    destinations: z.array(z.string()).optional(),
+    startDate:z.date().optional(),
+    endDate:z.date().optional(),
+    bookingDeadline:z.date().optional(),
+    price: z.number().positive().optional(),
+    capacity: z.number().positive().optional(),
+    availableSlots: z.number().positive().optional(),
+    status:z.enum(["completed", "on-going", "upcoming"]).optional(),
+    photo: z.string().optional(),
+    category:z.enum(["Economic" , "Standard" , "Premium"]).optional(),
+    includedServices:z.array(z.string()).optional(),
+    excludedServices:z.array(z.string()).optional(),
+    requiredEquipment:z.array(z.string()).optional(),
+    itinerary: z.array(ActivitiesValidation).optional(),    
+})
+
+export const eventValidations = {
+    createEventValidation,
+    updateEventValidation
+}

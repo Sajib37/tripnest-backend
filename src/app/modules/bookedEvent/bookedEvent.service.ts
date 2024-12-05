a70f8bf046cf0784129f7c6d21caff2e9506d0bf
sajib37 <sajibhasan9811@gmail.com>
2024-12-05 22:56:03 +0600

    event capacity update problenm solved when book or cancel

--- src/app/modules/bookedEvent/bookedEvent.service.ts
+++ src/app/modules/bookedEvent/bookedEvent.service.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import httpStatus from "http-status";
 import AppError from "../../errors/appError";
 import { User } from "../users/users.model";
 import { BookedEvent } from "./bookedEvent.model";
+import { Event } from "../event/event.model";
 
 const bookedEventIntoDB = async (userId: string, eventCode: string) => {
     const user = await User.findOne({ id: userId });
@@ -20,9 +22,26 @@ const bookedEventIntoDB = async (userId: string, eventCode: string) => {
             "You already booked this event !!"
         );
     }
+
+    const eventToBook = await Event.findOne({ _id:eventCode })
+    if (!eventToBook) {
+        throw new AppError(httpStatus.NOT_FOUND,"Event not found!!")
+    }
+    if (eventToBook?.capacity <= 0) {
+        throw new AppError(httpStatus.BAD_REQUEST,"Seat is full !!")
+    }
+
     const result = await BookedEvent.create({ userId: user._id, eventCode });
     if (!result) {
         throw new AppError(httpStatus.BAD_REQUEST, "Booked event failed !");
+    }
+    const updateEventCapacity = await Event.findOneAndUpdate(
+        { _id:eventCode },
+        { $inc: { capacity: -1 } },
+        { new: true }
+    );
+    if (!updateEventCapacity) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Event capacity update failed !");
     }
     return result;
 };
@@ -37,6 +56,14 @@ const cancelEventFromDB = async (userId: string, eventCode: string) => {
             httpStatus.NOT_FOUND,
             `No booked event found with userId: ${userId} and eventCode: ${eventCode}`
         );
+    }
+    const updateEventCapacity = await Event.findOneAndUpdate(
+        { _id:eventCode },
+        { $inc: { capacity: 1 } },
+        { new: true }
+    );
+    if (!updateEventCapacity) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Event capacity update failed !");
     }
     return result;
 };
@@ -53,14 +80,14 @@ const getMyEventsFromDB = async (userId: string) => {
     return result;
 };
 
-const getUserByEventFromDB = async(eventCode:string) =>{
-    const result = await BookedEvent.find({ eventCode }).populate('userId');
+const getUserByEventFromDB = async (eventCode: string) => {
+    const result = await BookedEvent.find({ eventCode }).populate("userId");
     return result;
-}
+};
 
 export const bookedEventServices = {
     bookedEventIntoDB,
     cancelEventFromDB,
     getMyEventsFromDB,
-    getUserByEventFromDB
+    getUserByEventFromDB,
 };
